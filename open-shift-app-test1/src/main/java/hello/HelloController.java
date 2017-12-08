43131a043273bb868c9aea1ad5b03785b80863ff
johan <johan@10.0.75.1>
2017-12-08 08:53:26 -0500

    Print env variables

--- open-shift-app-test1/src/main/java/hello/HelloController.java
+++ open-shift-app-test1/src/main/java/hello/HelloController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
     
-    @RequestMapping("/")
+    @RequestMapping("/test1/")
     public String index() {
-        return "Greetings from Spring Boot!";
+    	
+    	String out="";
+    	for(String k : System.getenv().keySet()) {
+    		String v=System.getenv(k);
+    		if(k!=null) out+=k+'='+v+"<br>";
+    	}
+        return "Greetings from Spring Boot!<br>"+out;
     }
     
 }
